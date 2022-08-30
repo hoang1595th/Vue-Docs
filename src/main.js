114d51f95e2efc97102c726d7160f857e28c772b
Hoang Nguyen Xuan <hoangnx1@smartosc.com>
2022-08-30 17:50:13 +0700

    Reactivity Fundamental

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.config.errorHandler = (err) => {
+  console.log("*****", err);
+}
+
+app.mount('#app')
