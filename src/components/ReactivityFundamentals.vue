499ce748675a9941237307ec04bebfe17b83a0b1
Hoang Nguyen Xuan <hoangnx1@smartosc.com>
2022-09-06 17:49:43 +0700

    List Rendering

---
+++ src/components/ReactivityFundamentals.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      firstName: 'John',
+      lastName: 'Doe'
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+  mounted() {
+    // methods can be called in lifecycle hooks, or other methods!
+    this.increment();
+    this.fullName = "nguyen hoang";
+  },
+  computed: {
+    publishedBooksMessage() {
+      return this.count % 2 === 0 ? 'Yes' : 'No'
+    },
+    fullName: {
+      // getter
+      get() {
+        return this.firstName + ' ' + this.lastName
+      },
+      // setter
+      set(newValue) {
+        console.log("newValue", newValue);
+        // Note: we are using destructuring assignment syntax here.
+        [this.firstName, this.lastName] = newValue.split(' ')
+      }
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <div>
+      {{ count }}
+    </div>
+    <div>
+      {{ publishedBooksMessage }}
+    </div>
+    <button @click="increment()">increment</button>
+    <hr>
+    <div>{{ fullName }}</div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+</style>
