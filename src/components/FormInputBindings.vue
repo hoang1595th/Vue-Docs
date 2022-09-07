66c62667088d27bcba277969af6c09a3dcdf1b07
Hoang Nguyen Xuan <hoangnx1@smartosc.com>
2022-09-07 18:03:28 +0700

    Lifecycle Hooks

---
+++ src/components/FormInputBindings.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data() {
+    return {
+      checkedNames: []
+    }
+  }
+}
+</script>
+
+<template>
+  <div>Checked names: {{ checkedNames }}</div>
+
+  <div class="main">
+    <div class="item">
+      <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+      <label for="jack">Jack</label>
+    </div>
+
+    <div class="item">
+      <input type="checkbox" id="john" value="John" v-model="checkedNames">
+      <label for="john">John</label>
+    </div>
+
+    <div class="item">
+      <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+      <label for="mike">Mike</label>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+main,
+.item {
+  display: flex;
+}
+</style>
